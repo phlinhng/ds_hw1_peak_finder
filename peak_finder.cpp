47e2f06c0ce2e85bb5fddf6891ceefcd617a5d02
phlin <phlinhng@outlook.com>
2018-10-11 20:05:47 +0800

    try vector

--- peak_finder.cpp
+++ peak_finder.cpp
@@ -1,64 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-int FindPeak(int a[][],int RowNum ,int ColNum){
-    int i,j,k;
-    k=1;
-    int temp[RowNum*ColNum][2];
-    for (i=0;i<RowNum;++i){
-        for (j=0;j<RowNum;++j){
-            if(a[i][j]>=a[i][j+1] && a[i][j]>=a[i][j-1]){
-                temp[k][0]=i;
-                temp[k][1]=j;
-                k++;
-            }
-        }
-    }
-    for (i=0;i<k;++i){
-        if(a[ temp[k][0] ][ temp [k][1] ])>a[ temp[k][0] +1 ][ temp [k][1]  ] &&
-        a[ temp[k][0] ][ temp [k][1] ])>a[ temp[k][0] -1 ][ temp [k][1]  ]){
-            temp2[k][0]=temp[k][0];
-            te
-        }
-    }
-}
+class Solution{
+    public:
+    void InputData();
+    int FindPeak;
+    void PrintResult();
+    private:
+    int row,col;
+    vector<vector<int>> mat;
+};
 
-int main(){
+void Solution::InputData(){
     ifstream fin;
     fin.open("matrix.data");
-    if(!fin) return 1;
-    int row,col;
     fin >> row;
     fin >> col;
-    int matrix[row][col];
+    mat.resize(row,vector<int>(col,0));
     int i,j;
     for(i=0;i<row;++i){
         for(j=0;j<col;++j){
-            fin >> matrix[i][j];
+            fin >> mat[i][j];
         }
     }
     fin.close();
-    ofstream fout;
-    fout.open("final.peak");
-    struct answer ans;
-    int peak_no=0;
+}
+
+void Solution::PrintResult(){
+    int i,j;
     for(i=0;i<row;++i){
-        for(j=0;j<row;++j){
-            if(result[i][j]==1){
-                ans.peak_r[peak_no]=i+1;
-                ans.peak_c[peak_no]=j+1;
-                peak_no++;
-            }
+        for(j=0;j<col;++j){
+            cout << mat[i][j] << ' ';
         }
+        cout << endl;
     }
-    ans.peak_num=peak_no;
-    fout << ans.peak_num;
-    fout << "\n";
-    for(i=0;i<peak_no;++i){
-        fout << ans.peak_r[i] << ' ' << ans.peak_c[i];
-        fout << "\n";
-    }
-    fout.close();
+}
+
+int main(int argc, char *argv[]){
+    Solution sol;
+    sol.InputData();
+    sol.PrintResult();
     return 0;
 }
