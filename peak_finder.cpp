84d3a11bc41614c578084677090fe458f3476869
phlin <phlinhng@outlook.com>
2018-10-11 21:17:54 +0800

    some updates

--- peak_finder.cpp
+++ peak_finder.cpp
@@ -6,12 +6,35 @@ using namespace std;
 class Solution{
     public:
     void InputData();
-    int FindPeak;
+    void FindPeak();
     void PrintResult();
     private:
     int row,col;
     vector<vector<int>> mat;
+    vector<int> ans_row;
+    vector<int> ans_col;
+    vector<int> peak_row;
+    vector<int> peak_col;
 };
+
+void Solution::FindPeak(){
+    int i,j;
+    for(i=0;i<row;++i){
+        for(j=0;j<col;++j){
+            if (mat[i][j] >= mat[i][j+1] && mat[i][j] >= mat[i][j-1]){
+                ans_row.push_back(i);
+                ans_col.push_back(j);
+            }
+        }
+    }
+    for(i=0;i<ans_row.size();++i){
+        if (mat[ans_row[i]][ans_col[i]] >= mat[ans_row[i]+1][ans_col[i]] && 
+        mat[ans_row[i]][ans_col[i]] >= mat[ans_row[i]-1][ans_col[i]]){
+            peak_row.push_back(ans_row[i]);
+            peak_col.push_back(ans_col[i]);
+        }
+    }
+}
 
 void Solution::InputData(){
     ifstream fin;
@@ -29,18 +52,16 @@ void Solution::InputData(){
 }
 
 void Solution::PrintResult(){
-    int i,j;
-    for(i=0;i<row;++i){
-        for(j=0;j<col;++j){
-            cout << mat[i][j] << ' ';
-        }
-        cout << endl;
+    cout << peak_row.size() << endl;
+    for(int i=0;i<peak_row.size();++i){
+        cout << peak_row[i] << peak_col[i] << endl;
     }
 }
 
-int main(int argc, char *argv[]){
+int main(){
     Solution sol;
     sol.InputData();
+    sol.FindPeak();
     sol.PrintResult();
     return 0;
 }
